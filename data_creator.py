241c792e5ce96e86a25a5b276ebc3549c01cbfe8
Robert <rtedwards.eng@gmail.com>
2020-11-01 22:27:04 +0000

    created dataset

--- data_creator.py
+++ data_creator.py
@@ -4,5 +4,10 @@ from faker import Faker
 
 fake = Faker()
 
-names = [fake.name() for i in range 100]
+names = [fake.name() for i in range(100)]
+cities = [fake.city() for i in range(100)]
 ages = np.random.random_integers(0, 100, size=100)
+
+df = pd.DataFrame({"name": names, "age": ages, "city": cities})
+
+df.to_feather("_data/test_data.feather")
